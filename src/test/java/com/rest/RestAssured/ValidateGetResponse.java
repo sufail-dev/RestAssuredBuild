17eb8f3e48a980f9ccaad430b58e85fc0eb42ce6
myofficialthing@gmail.com <myofficialthing@gmail.com>
2022-02-13 07:19:01 +0530

    updated key

--- src/test/java/com/rest/RestAssured/ValidateGetResponse.java
+++ src/test/java/com/rest/RestAssured/ValidateGetResponse.java
@@ -12,7 +12,7 @@ public class ValidateGetResponse {
 		given()
 		.baseUri("https://api.getpostman.com")
 		.header("X-Api-Key","\r\n" + 
-				"PMAK-6203df990dd32b308e6dcf70-e0272052d58a52b561d45ba52459f03a0f").
+				"").
 		
 		when()
 		.get("/workspaces").
